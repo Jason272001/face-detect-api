270a1c56b24115b07a9f6ea8c66fa67691e896e3
Jason272001 <93046082+Jason272001@users.noreply.github.com>
2024-03-06 23:23:00 -0800

    asd

--- server.js
+++ server.js
@@ -1,42 +1,48 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 const app = express();
-const knex = require('knex');
+const knex = require("knex");
 
-const signin = require('./controllers/signin');
-const signup = require('./controllers/signup');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
-    const db=knex({
-  client: 'pg',
+const signin = require("./controllers/signin");
+const signup = require("./controllers/signup");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+const db = knex({
+  client: "pg",
   connection: {
-    host : '127.0.0.1',
+    host: "127.0.0.1",
 
-    user : 'postgres',
-    password : 'MeMe1965',
-    database : 'smart-brain'
-  }
+    user: "postgres",
+    password: "MeMe1965",
+    database: "smart-brain",
+  },
 });
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("success");
+});
 
-app.get('/', (req, res)=> {
-    res.send(database.users);
-})
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
 
-app.post('/signin', (req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
+app.post("/register", (req, res) => {
+  signup.handleSignup(req, res, db, bcrypt);
+});
 
-app.post('/register',  (req,res)=>{signup.handleSignup(req,res,db,bcrypt)})
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfile(req, res, db);
+});
 
-app.get('/profile/:id',(req,res)=>{profile.handleProfile(req,res,db)} )
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
 
-app.put('/image',(req,res)=>{image.handleImage(req,res,db)} )
-
-
-app.listen(3000, () => {
-    console.log('app runing port 3000')
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app runing port ${process.env.PORT}`);
+});
